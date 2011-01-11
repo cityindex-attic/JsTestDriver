5b9e073def4fd1cb4daf47b41d975c535e40fefb
robertsdionne <robertsdionne@205c1ca0-44cf-11de-84a6-839b6ed6c72e>
2011-01-11 22:06:31 +0000

    Adding a conditional asynchronous test case. Adds more tests.
    git-svn-id: http://js-test-driver.googlecode.com/svn/trunk@821 205c1ca0-44cf-11de-84a6-839b6ed6c72e

--- JsTestDriver/src-test/com/google/jstestdriver/javascript/TestCaseBuilderTest.js
+++ JsTestDriver/src-test/com/google/jstestdriver/javascript/TestCaseBuilderTest.js
@@ -13,10 +13,11 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-var TestCaseBuilderTest = jstestdriver.testCaseManager.TestCase('TestCaseBuilderTest');
+var TestCaseBuilderTest = TestCase('TestCaseBuilderTest');
 
 
-TestCaseBuilderTest.prototype.testCreatedTestCasesAddedToTestCaseManager = function() {
+TestCaseBuilderTest.prototype.testCreatedTestCasesAddedToTestCaseManager =
+    function() {
   var testCaseManager = new jstestdriver.TestCaseManager();
   var testCaseBuilder = new jstestdriver.TestCaseBuilder(testCaseManager);
   var testCase = testCaseBuilder.TestCase('testCase');
@@ -28,4 +29,77 @@ TestCaseBuilderTest.prototype.testCreatedTestCasesAddedToTestCaseManager = funct
   assertEquals(jstestdriver.TestCaseInfo.DEFAULT_TYPE, testCaseInfo.getType());
   assertEquals('testCase', testCaseInfo.getTestCaseName())
   assertSame(testCase, testCaseInfo.getTemplate());
+};
+
+
+TestCaseBuilderTest.prototype.testAsyncTestCasesAddedToManager = function() {
+  var testCaseManager = new jstestdriver.TestCaseManager();
+  var testCaseBuilder = new jstestdriver.TestCaseBuilder(testCaseManager);
+  var testCase = testCaseBuilder.AsyncTestCase('asyncTestCase');
+  var testCaseInfos = testCaseManager.getTestCasesInfo();
+
+  assertEquals(1, testCaseInfos.length);
+  var testCaseInfo = testCaseInfos[0];
+
+  assertEquals(jstestdriver.TestCaseInfo.ASYNC_TYPE, testCaseInfo.getType());
+  assertEquals('asyncTestCase', testCaseInfo.getTestCaseName());
+  assertEquals(testCase, testCaseInfo.getTemplate());
+};
+
+
+TestCaseBuilderTest.prototype.testConditionalTestCasesAddedToManager =
+    function() {
+  var testCaseManager = new jstestdriver.TestCaseManager();
+  var testCaseBuilder = new jstestdriver.TestCaseBuilder(testCaseManager);
+  var trueTestCase =
+      testCaseBuilder.ConditionalTestCase('trueTestCase', function() {
+        return true
+      });
+  var falseTestCase =
+      testCaseBuilder.ConditionalTestCase('falseTestCase', function() {
+        return false;
+      });
+  var testCaseInfos = testCaseManager.getTestCasesInfo();
+
+  assertEquals(2, testCaseInfos.length);
+  var trueTestInfo = testCaseInfos[0];
+  var falseTestInfo = testCaseInfos[1];
+
+  assertEquals(jstestdriver.TestCaseInfo.DEFAULT_TYPE, trueTestInfo.getType());
+  assertEquals('trueTestCase', trueTestInfo.getTestCaseName());
+  assertEquals(trueTestCase, trueTestInfo.getTemplate());
+
+  assertEquals(jstestdriver.TestCaseInfo.DEFAULT_TYPE, falseTestInfo.getType());
+  assertEquals('falseTestCase', falseTestInfo.getTestCaseName());
+  assertEquals(
+      jstestdriver.TestCaseBuilder.PlaceHolderCase, falseTestInfo.getTemplate());
+};
+
+
+TestCaseBuilderTest.prototype.testConditionalAsyncTestCasesAddedToManager =
+    function() {
+  var testCaseManager = new jstestdriver.TestCaseManager();
+  var testCaseBuilder = new jstestdriver.TestCaseBuilder(testCaseManager);
+  var trueTestCase =
+      testCaseBuilder.ConditionalAsyncTestCase('trueTestCase', function() {
+        return true
+      });
+  var falseTestCase =
+      testCaseBuilder.ConditionalAsyncTestCase('falseTestCase', function() {
+        return false;
+      });
+  var testCaseInfos = testCaseManager.getTestCasesInfo();
+
+  assertEquals(2, testCaseInfos.length);
+  var trueTestInfo = testCaseInfos[0];
+  var falseTestInfo = testCaseInfos[1];
+
+  assertEquals(jstestdriver.TestCaseInfo.ASYNC_TYPE, trueTestInfo.getType());
+  assertEquals('trueTestCase', trueTestInfo.getTestCaseName());
+  assertEquals(trueTestCase, trueTestInfo.getTemplate());
+
+  assertEquals(jstestdriver.TestCaseInfo.ASYNC_TYPE, falseTestInfo.getType());
+  assertEquals('falseTestCase', falseTestInfo.getTestCaseName());
+  assertEquals(
+      jstestdriver.TestCaseBuilder.PlaceHolderCase, falseTestInfo.getTemplate());
 };
--- JsTestDriver/src/com/google/jstestdriver/javascript/TestCaseBuilder.js
+++ JsTestDriver/src/com/google/jstestdriver/javascript/TestCaseBuilder.js
@@ -1,24 +1,47 @@
 /*
- * Copyright 2009 Google Inc.
- * 
+ * Copyright 2011 Google Inc.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
  */
+
+/**
+ * @fileoverview
+ * @author corysmith@google.com (Cory Smith)
+ * @author rdionne@google.com (Robert Dionne)
+ */
+
+
+
+/**
+ * Constructs a TestCaseBuilder.
+ * @param {jstestdriver.TestCaseManager} testCaseManager The appropriate test
+ *     case manager.
+ * @constructor
+ */
 jstestdriver.TestCaseBuilder = function(testCaseManager) {
   this.testCaseManager_ = testCaseManager;
 };
 
 
-jstestdriver.TestCaseBuilder.prototype.TestCase = function(testCaseName, opt_proto, opt_type) {
+/**
+ * Defines a test case.
+ * @param {string} testCaseName The name of the test case.
+ * @param {Object} opt_proto An optional prototype.
+ * @param {Object} opt_type Either DEFAULT_TYPE or ASYNC_TYPE.
+ * @return {Function} Base function that represents the test case class.
+ */
+jstestdriver.TestCaseBuilder.prototype.TestCase =
+    function(testCaseName, opt_proto, opt_type) {
   var testCaseClass = function() {};
 
   if (opt_proto) {
@@ -30,36 +53,68 @@ jstestdriver.TestCaseBuilder.prototype.TestCase = function(testCaseName, opt_pro
   if (typeof testCaseClass.prototype.tearDown == 'undefined') {
     testCaseClass.prototype.tearDown = function() {};
   }
-  this.testCaseManager_.add(new jstestdriver.TestCaseInfo(testCaseName, testCaseClass, opt_type));
+  this.testCaseManager_.add(
+      new jstestdriver.TestCaseInfo(testCaseName, testCaseClass, opt_type));
   return testCaseClass;
 };
 
 
-jstestdriver.TestCaseBuilder.prototype.AsyncTestCase = function(testCaseName, opt_proto) {
-  return this.TestCase(testCaseName, opt_proto, jstestdriver.TestCaseInfo.ASYNC_TYPE);
+/**
+ * Defines an asynchronous test case.
+ * @param {string} testCaseName The name of the test case.
+ * @param {Object} opt_proto An optional prototype.
+ * @return {Function} Base function that represents the asyncronous test case
+ *     class.
+ */
+jstestdriver.TestCaseBuilder.prototype.AsyncTestCase =
+    function(testCaseName, opt_proto) {
+  return this.TestCase(
+      testCaseName, opt_proto, jstestdriver.TestCaseInfo.ASYNC_TYPE);
 };
 
 
 /**
  * A TestCase that will only be executed when a certain condition is true.
- * @param {String} The name of the TestCase.
- * @param {function():boolean} A function that indicates if this case should be run.
+ * @param {string} The name of the TestCase.
+ * @param {function():boolean} A function that indicates if this case should be
+ *     run.
+ * @param {Object} opt_proto An optional prototype for the test case class.
+ * @param {Object} opt_type Either DEFAULT_TYPE or ASYNC_TYPE.
  * @return {Function} Base function that represents the TestCase class.
  */
-jstestdriver.TestCaseBuilder.prototype.ConditionalTestCase = function(testCaseName, condition, opt_proto) {
+jstestdriver.TestCaseBuilder.prototype.ConditionalTestCase =
+    function(testCaseName, condition, opt_proto, opt_type) {
   if (condition()) {
-    return this.TestCase(testCaseName, opt_proto, jstestdriver.TestCaseInfo.DEFAULT_TYPE);
+    return this.TestCase(testCaseName, opt_proto, opt_type);
   }
-  var testCaseClass =
-      jstestdriver.TestCaseBuilder.PlaceHolderCase;
   this.testCaseManager_.add(
-      new jstestdriver.TestCaseInfo(testCaseName,
-              testCaseClass,
-          jstestdriver.TestCaseInfo.DEFAULT_TYPE));
+      new jstestdriver.TestCaseInfo(
+          testCaseName,
+          jstestdriver.TestCaseBuilder.PlaceHolderCase,
+          opt_type));
   return function(){};
 };
 
 
+/**
+ * An AsyncTestCase that will only be executed when a certain condition is true.
+ * @param {String} The name of the AsyncTestCase.
+ * @param {function():boolean} A function that indicates if this case should be
+ *     run.
+ * @param {Object} opt_proto An optional prototype for the test case class.
+ * @return {Function} Base function that represents the TestCase class.
+ */
+jstestdriver.TestCaseBuilder.prototype.ConditionalAsyncTestCase =
+    function(testCaseName, condition, opt_proto) {
+  return this.ConditionalTestCase(
+      testCaseName, condition, opt_proto, jstestdriver.TestCaseInfo.ASYNC_TYPE);
+};
+
+
+/**
+ * Constructs a place holder test case.
+ * @constructor
+ */
 jstestdriver.TestCaseBuilder.PlaceHolderCase = function() {};
 
 
